3c309425174ac79b07b80655cee7d2f55477ab88
ytyaru <yttry0@gmail.com>
2021-01-23 16:54:24 +0900

    @inputはできた。@load,@beforeunloadはできなかった。

---
+++ docs/3/color-radio-button-list-component.js
@@ -0,0 +1,15 @@
+var ColorRadioButtonComponent = {
+    template: `
+        <radio-button-list-component
+            name="color"
+            checked_value="black"
+            required="false"
+            :list="[
+                {id:'setting-color-white', value:'white', title:'背景白', text:'白'},
+                {id:'setting-color-black', value:'black', title:'背景黒', text:'黒'},
+                {id:'setting-color-red',   value:'red',   title:'背景赤', text:'赤'},
+            ]"
+        ></radio-button-list-component>
+    `,
+}
+
